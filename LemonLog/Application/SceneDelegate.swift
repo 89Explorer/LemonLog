d5386e6f39abc86478243ebd0f101f8d88618f36
89Explorer <https://github.com/89Explorer>
2025-10-15 10:34:42 +0900

    feat: MVVM 기반 폴더 구조 설계 및 프로젝트 구조 정리

---
+++ LemonLog/Application/SceneDelegate.swift
@@ -0,0 +1,65 @@
+//
+//  SceneDelegate.swift
+//  LemonLog
+//
+//  Created by 권정근 on 10/12/25.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        // 1. Scene 가져오기 (필수)
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // 2. 윈도우 생성 및 Scene 연결 (최신 방식)
+        window = UIWindow(windowScene: windowScene)
+        
+        // 3. 루트 뷰 컨트롤러. 설정
+        let mainVC = ViewController()
+        window?.rootViewController = mainVC
+        
+        // 4. 화면에 표시 (필수)
+        window?.makeKeyAndVisible()
+        
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) {
+        // Called as the scene is being released by the system.
+        // This occurs shortly after the scene enters the background, or when its session is discarded.
+        // Release any resources associated with this scene that can be re-created the next time the scene connects.
+        // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+    }
+
+    func sceneDidBecomeActive(_ scene: UIScene) {
+        // Called when the scene has moved from an inactive state to an active state.
+        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
+    }
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        // Called when the scene will move from an active state to an inactive state.
+        // This may occur due to temporary interruptions (ex. an incoming phone call).
+    }
+
+    func sceneWillEnterForeground(_ scene: UIScene) {
+        // Called as the scene transitions from the background to the foreground.
+        // Use this method to undo the changes made on entering the background.
+    }
+
+    func sceneDidEnterBackground(_ scene: UIScene) {
+        // Called as the scene transitions from the foreground to the background.
+        // Use this method to save data, release shared resources, and store enough scene-specific state information
+        // to restore the scene back to its current state.
+
+        // Save changes in the application's managed object context when the application transitions to the background.
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
+    }
+
+
+}
+
